4071b673c07ffcee94787bf53f3c8cc10297a501
Paul Harbinson <baronlushpain@icloud.com>
2021-02-11 22:11:58 +0000

    call GraphQL from the React UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
 
 function App() {
+  const [notes, updateNotes] = useState([])
+
+  const query = `
+    query listNotes {
+      listNotes {
+        id title content username
+      }
+    }
+  `
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  async function fetchNotes() {
+    try {
+      const noteData = await API.graphql({ query })
+      console.log('data from GraphQL: ', noteData)
+      updateNotes(noteData.data.listNotes.items)
+    } catch (err) {
+      console.log('error fetching notes...', err)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        notes.map((note, index) => (
+          <div key={index}>
+            <h3>{note.username}</h3>
+            <h5>{note.title}</h5>
+            <p>{note.content}</p>
+          </div>
+        ))
+      }
     </div>
   );
+
 }
 
 export default App;

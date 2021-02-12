dd953e0ee1d92e9af516172f5d1a4ced18f56425
Paul Harbinson <baronlushpain@icloud.com>
2021-02-12 00:14:04 +0000

    update state

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
   async function fetchPosts() {
     try {
       const postData = await API.graphql({ query })
-      console.log('data from GraphQL: ', postData)
-      updatePosts(postData.data.listPosts.items)
+      console.log('data from GraphQL: ', postData.data.listPosts)
+      updatePosts(postData.data.listPosts)
     } catch (err) {
       console.log('error fetching posts...', err)
     }
@@ -32,7 +32,6 @@ function App() {
       {
         posts.map((post, index) => (
           <div key={index}>
-            <h1>BY FUCK!</h1>
             <h3>{post.username}</h3>
             <h5>{post.title}</h5>
             <p>{post.content}</p>
